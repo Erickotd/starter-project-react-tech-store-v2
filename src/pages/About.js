f3db5ec910d0bffb10e688dd4f17413e6d197d66
erickotd <erickotd@gmail.com>
2020-12-01 22:30:02 +0100

    add Hero

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React from 'react';
 
 export default function About() {
-  return <h1>hello from about page</h1>;
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        I'm baby helvetica gentrify next level disrupt actually austin.
+        Thundercats YOLO art party, ramps hot chicken trust fund yuccie shaman
+        church-key messenger bag everyday carry tumeric small batch ethical
+        meggings. Godard vape blue bottle, taxidermy slow-carb narwhal chambray
+        aesthetic authentic. Venmo live-edge man braid, man bun four dollar
+        toast lumbersexual butcher pug austin tote bag offal mlkshk deep v
+        enamel pin.
+      </p>
+    </section>
+  );
 }
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 export default function Products() {
   return <h1>hello from products page</h1>;
