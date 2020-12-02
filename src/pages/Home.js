00503268aa2ebbcff6dd5dbb90441e51625ebb1a
erickotd <erickotd@gmail.com>
2020-12-02 22:16:50 +0100

    features finish

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Hero from '../components/Hero';
 import { Link } from 'react-router-dom';
+import FeaturedProducts from '../components/Products/FeaturedProducts';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
           our products
         </Link>
       </Hero>
+      <FeaturedProducts />
     </>
   );
 }
--- src/utils/URL.js
+++ src/utils/URL.js
@@ -1,1 +1,3 @@
 // url
+
+export default 'http://206.189.182.129/projects/react-tech-store-v2';
